b4189e392020656ffe6d8a92caf90a8307468791
Nathan Martins <nathan.eua@gmail.com>
2019-12-04 09:58:57 -0300

    adding add test.

---
+++ tests/test_add.py
@@ -0,0 +1,42 @@
+import argparse
+import os
+import shutil
+import unittest
+
+from aurum import base, git
+
+
+class AddTestCase(unittest.TestCase):
+
+    def setUp(self) -> None:
+        super().setUp()
+        base.run_init(argparse.Namespace())
+        self.file_path = os.path.abspath("README.md")
+
+    def tearDown(self) -> None:
+        super().tearDown()
+        for path in base.DEFAULT_DIRS:
+            shutil.rmtree(path, ignore_errors=True)
+
+    def test_add(self):
+        parser = argparse.Namespace(files=[
+            self.file_path,
+        ])
+        base.run_add(parser)
+
+        proc = git.run_git(
+            "status"
+        )
+
+        self.assertEqual(proc.wait(), 0)
+        prod_res = proc.stdout.read()
+
+        self.assertIn(b"new file:   README.md", prod_res)
+
+        meta_data_file_path = os.path.join(".au", os.listdir(".au")[0])
+        meta_data_assert = f'new file:   {meta_data_file_path}'
+        self.assertIn(str.encode(meta_data_assert), prod_res)
+
+
+if __name__ == '__main__':
+    unittest.main()
