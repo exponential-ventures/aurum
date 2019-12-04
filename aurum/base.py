633dbd84b290fbd71c8b812baf643ff9c8b51f43
Nathan Martins <nathan.eua@gmail.com>
2019-12-03 22:31:55 -0300

    Adding complete add functionality.

--- aurum/base.py
+++ aurum/base.py
@@ -21,16 +21,19 @@
 ##    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 ##
 import argparse
+import hashlib
+import json
 import logging
 import os
 import sys
+from datetime import datetime
 from pathlib import Path
 
 from aurum import git
 
 cwd = Path(os.getcwd())
 
-DEFAULT_DIRS = [cwd / ".au.py", cwd / "src", cwd / "logs"]
+DEFAULT_DIRS = [cwd / ".au", cwd / "src", cwd / "logs"]
 
 
 def execute_commands(parser: argparse.Namespace):
@@ -39,6 +42,10 @@ def execute_commands(parser: argparse.Namespace):
     logging.debug("Parser arguments: {}".format(parser))
 
     git.check_git()
+
+    if not hasattr(parser, "subcommand"):
+        sys.stderr.write(f"No command was passed in \n")
+        sys.exit(1)
 
     if parser.subcommand == "init":
         run_init(parser)
@@ -59,13 +66,56 @@ def run_init(parser: argparse.Namespace):
 def run_add(parser: argparse.Namespace):
     logging.debug(f"Adding files to aurum: {parser.files}")
 
+    if not os.path.exists('.au'):
+        sys.stderr.write(f"Path '.au' does not exist, please run au init \n")
+        sys.exit(1)
+
+    for file in parser.files:
+        if not os.path.exists(file):
+            sys.stderr.write(f"Path '{file}' does not exist! \n")
+            sys.exit(1)
+
+        if not os.path.isfile(file):
+            sys.stderr.write(f"Path '{file}' must be a file! \n")
+            sys.exit(1)
+
+        # File Hashing
+        # Using buffers to not use tons of memory.
+        sha1 = hashlib.sha1()
+        buf_size = 65536  # lets read stuff in 64kb chunks!
+
+        with open(file, 'rb') as f:
+            while True:
+                data = f.read(buf_size)
+                if not data:
+                    break
+                sha1.update(data)
+
+        meta_data = {
+            "file_name": file,
+            "added": str(datetime.now()),
+            "size": os.path.getsize(file),
+            "hash": sha1.hexdigest(),
+            "parent_hash": None,
+        }
+
+        meta_data_str = json.dumps(meta_data)
+
+        meta_data_file_name = hashlib.sha1()
+        meta_data_file_name.update(str.encode(meta_data_str))
+        meta_data_file_name = meta_data_file_name.hexdigest()
+        meta_data_file_name = os.path.join(".au", meta_data_file_name)
+
+        with open(meta_data_file_name, "w+") as f:
+            f.write(meta_data_str)
+
 
 def create_default_dirs():
     for path in DEFAULT_DIRS:
         if path.exists():
             logging.error("Can't create {} directory. Already exists.".format(path))
             sys.exit(1)
-
+        logging.debug(f"Creating dir {path}")
         os.makedirs(path)
 
 
