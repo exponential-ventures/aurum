00f691eb3d3bed14d06b20318c5b1b0f4553e8a8
Thales Ribeiro <thalessr@gmail.com>
2019-12-05 14:33:28 +0200

    Adding command line arguments taking precedence

--- aurum/__init__.py
+++ aurum/__init__.py
@@ -2,7 +2,7 @@
 ##
 ## Authors: Adriano Marques
 ##          Nathan Martins
-##          Thales Ribeiro 
+##          Thales Ribeiro
 ##
 ## Copyright (C) 2019 Exponential Ventures LLC
 ##
@@ -27,4 +27,42 @@ __license__ = "GNU LESSER GENERAL PUBLIC LICENSE 2.0"
 __url__ = "https://github.com/exponential-ventures/aurum"
 __version__ = "0.1"
 
+import sys
+import argparse
+
 from aurum.base import execute_commands
+
+# params = ['a', 'b', 'batch_size']
+
+def parameters(**kwargs):
+    # setattr(sys.modules[__name__], 'params', list(kwargs.keys()))
+    parse_params = main(list(kwargs.keys()))
+    for key in kwargs.keys():
+        value = None
+        try:
+            if key in parse_params:
+                value = parse_params.__getattribute__(key)
+        except:
+            pass
+
+        setattr(sys.modules[__name__], key, value or kwargs[key])
+
+def main(params):
+    description = """Aurum is a new and simplified approach for data scientists to
+    keep track of data and code without having to get another PhD for it. Aurum
+    keeps track of all code and data changes, and lets you easily reproduce any
+    experiment as well as easily compare metrics across experiments.
+    """
+    epilog = "And that's how you make your live easier. You're welcome."
+    parser = argparse.ArgumentParser(description=description, epilog=epilog, add_help=True)
+
+    for param in params:
+        parser.add_argument(f'-{param}', required=False)
+
+    # setattr(sys.modules[__name__], 'parse_params',  parser.parse_args())
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    main()
+
+# main()
---
+++ examples/src/experiment.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# import aurum as au
+try:
+    import aurum as au
+except ImportError:
+    import sys
+
+    sys.path.append("../aurum")
+    import aurum as au
+
+au.parameters(a=0.01, b=1000, c=46, epochs=100, batch_size=200)
+
+
+print(f"Parameter a = {au.a}")
+print(f"Parameter b = {au.b}")
+print(f"Parameter epochs = {au.epochs}")
+print(f"Parameter batch_size = {au.batch_size}")
