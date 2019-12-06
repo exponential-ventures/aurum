771b8be78393c9cd9ee0aee241511a36f3a39acd
Nathan Martins <nathan.eua@gmail.com>
2019-12-06 09:59:57 -0300

    Moving a few functions to utils.

--- aurum/metadata/dataset_meta_data.py
+++ aurum/metadata/dataset_meta_data.py
@@ -5,6 +5,7 @@ from datetime import datetime
 
 from aurum.constants import DATASET_METADATA_DIR
 from aurum.metadata import MetaData
+from aurum.utils import gen_file_hash, make_safe_filename
 
 
 class DatasetMetaData(MetaData):
@@ -27,7 +28,7 @@ class DatasetMetaData(MetaData):
 
         if file_name != '':
             with open(file_name, 'r') as f:
-                self.serialize(f.read())
+                self.deserialize(f.read())
 
     def deserialize(self, raw_json: str):
         super().deserialize(raw_json)
@@ -36,41 +37,13 @@ class DatasetMetaData(MetaData):
     def save(self, destination: str = None) -> None:
 
         if destination is None:
-            meta_data_str = json.dumps(self.deserialize())
-            destination = self.gen_meta_file_name(meta_data_str)
+            meta_data_str = json.dumps(self.serialize())
+            destination = gen_meta_file_name(meta_data_str, self.file_name)
+
+        if self.file_hash is None:
+            self.file_hash = gen_file_hash(self.file_name)
 
         super().save(destination)
-
-    def gen_file_hash(self):
-        sha1 = hashlib.sha1()
-        buf_size = 65536  # lets read stuff in 64kb chunks!
-
-        with open(self.file_name, 'rb') as f:
-            while True:
-                data = f.read(buf_size)
-                if not data:
-                    break
-                sha1.update(data)
-
-        self.file_hash = sha1.hexdigest()
-
-    def gen_meta_file_name(self, meta_data_str):
-
-        meta_data_dir = os.path.join(DATASET_METADATA_DIR, make_safe_filename(self.file_name))
-
-        if not os.path.exists(meta_data_dir):
-            os.mkdir(meta_data_dir)
-
-        meta_hash = self.gen_meta_hash(meta_data_str)
-        meta_data_file_name = meta_hash + ".json"
-
-        return os.path.join(meta_data_dir, meta_data_file_name)
-
-    @staticmethod
-    def gen_meta_hash(meta_data_str):
-        meta_data_file_name = hashlib.sha1()
-        meta_data_file_name.update(str.encode(meta_data_str))
-        return meta_data_file_name.hexdigest()
 
 
 def get_dataset_metadata(file_name: str) -> (str, DatasetMetaData):
@@ -88,11 +61,19 @@ def get_dataset_metadata(file_name: str) -> (str, DatasetMetaData):
     return None, None
 
 
-def make_safe_filename(s):
-    def safe_char(c):
-        if c.isalnum():
-            return c
-        else:
-            return "_"
+def gen_meta_hash(meta_data_str):
+    meta_data_file_name = hashlib.sha1()
+    meta_data_file_name.update(str.encode(meta_data_str))
+    return meta_data_file_name.hexdigest()
 
-    return "".join(safe_char(c) for c in s).rstrip("_")
+
+def gen_meta_file_name(meta_data_str, file_name):
+    meta_data_dir = os.path.join(DATASET_METADATA_DIR, make_safe_filename(file_name))
+
+    if not os.path.exists(meta_data_dir):
+        os.mkdir(meta_data_dir)
+
+    meta_hash = gen_meta_hash(meta_data_str)
+    meta_data_file_name = meta_hash + ".json"
+
+    return os.path.join(meta_data_dir, meta_data_file_name)
