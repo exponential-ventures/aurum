90c57398d8396dd0ab414beeb31f8ea1395ef052
Thales Ribeiro <thalessr@gmail.com>
2019-12-10 16:13:25 -0300

    Add test case when argument is not accepted

--- tests/test_parameters.py
+++ tests/test_parameters.py
@@ -2,7 +2,9 @@ import unittest
 import argparse
 import shutil
 import sys
+import os
 import tracemalloc
+import subprocess
 from io import StringIO
 from unittest.mock import patch
 
@@ -21,17 +23,15 @@ class TestParameters(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         tracemalloc.start()
+        cls.current_dir = os.getcwd()
+        au.base.run_init(argparse.Namespace())
 
     @classmethod
     def tearDownClass(cls):
-        tracemalloc.stop()
-
-    def setUp(self):
-        au.base.run_init(argparse.Namespace())
-
-    def tearDown(self):
         for path in au.base.DEFAULT_DIRS:
             shutil.rmtree(path, ignore_errors=True)
+
+        tracemalloc.stop()
 
     def test_parameters(self):
         au.parameters(a=1.01, b=333, c=46, epochs=100, batch_size=200)
@@ -42,7 +42,7 @@ class TestParameters(unittest.TestCase):
         self.assertEqual(au.batch_size, 200)
 
     def test_parameters_from_arg(self):
-        args = argparse.Namespace(a=300)
+        args = argparse.Namespace(a=300, b=None, c=None, epochs=None, batch_size=None)
         with patch('aurum.command_line_args', return_value=args):
             au.parameters(a=1.01, b=333, c=46, epochs=100, batch_size=200)
             self.assertEqual(au.a, 300)
@@ -51,6 +51,32 @@ class TestParameters(unittest.TestCase):
             self.assertEqual(au.epochs, 100)
             self.assertEqual(au.batch_size, 200)
 
+    def test_parameters_from_arg_wo_mock(self):
+        proc = subprocess.Popen(
+            [f"python3 {self.current_dir}/examples/src/experiment.py -a 40 -epochs 99", ],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            shell=True,
+            cwd=self.current_dir,
+        )
+
+        output, _ = proc.communicate()
+
+        self.assertTrue(b'a = 40' in output)
+        self.assertTrue(b'epochs = 99' in output)
+
+    def test_parameters_with_wrong_argument(self):
+
+        proc = subprocess.Popen(
+            [f"python3 {self.current_dir}/examples/src/experiment.py -resga 40", ],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            shell=True,
+            cwd=self.current_dir,
+        )
+
+        _, error = proc.communicate()
+        self.assertTrue(b'error: unrecognized arguments: -resga 40\n' in error)
 
 if __name__ == '__main__':
     unittest.main()
