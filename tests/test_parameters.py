6fa23ddeb5834c8d5405971b25045dec241f5d23
Thales Ribeiro <thalessr@gmail.com>
2019-12-10 14:19:54 -0300

    Add a basic test

---
+++ tests/test_parameters.py
@@ -0,0 +1,56 @@
+import unittest
+import argparse
+import shutil
+import sys
+import tracemalloc
+from io import StringIO
+from unittest.mock import patch
+
+try:
+    import aurum as au
+    from aurum.constants import REPOSITORY_DIR
+except ImportError:
+    import sys
+
+    sys.path.append("../aurum")
+    import aurum as au
+    from aurum.constants import REPOSITORY_DIR
+
+
+class TestParameters(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        tracemalloc.start()
+
+    @classmethod
+    def tearDownClass(cls):
+        tracemalloc.stop()
+
+    def setUp(self):
+        au.base.run_init(argparse.Namespace())
+
+    def tearDown(self):
+        for path in au.base.DEFAULT_DIRS:
+            shutil.rmtree(path, ignore_errors=True)
+
+    def test_parameters(self):
+        au.parameters(a=1.01, b=333, c=46, epochs=100, batch_size=200)
+        self.assertEqual(au.a, 1.01)
+        self.assertEqual(au.b, 333)
+        self.assertEqual(au.c, 46)
+        self.assertEqual(au.epochs, 100)
+        self.assertEqual(au.batch_size, 200)
+
+    def test_parameters_from_arg(self):
+        args = argparse.Namespace(a=300)
+        with patch('aurum.command_line_args', return_value=args):
+            au.parameters(a=1.01, b=333, c=46, epochs=100, batch_size=200)
+            self.assertEqual(au.a, 300)
+            self.assertEqual(au.b, 333)
+            self.assertEqual(au.c, 46)
+            self.assertEqual(au.epochs, 100)
+            self.assertEqual(au.batch_size, 200)
+
+
+if __name__ == '__main__':
+    unittest.main()
