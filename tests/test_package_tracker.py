8f0166f34bfa055a78311a924ec489be34ed43fb
Nathan Martins <nathan.eua@gmail.com>
2019-12-12 08:55:58 -0300

    Better comment

--- tests/test_package_tracker.py
+++ tests/test_package_tracker.py
@@ -10,9 +10,7 @@ from aurum.package_tracker import installed_packages_hash
 class PackageTrackerTestCase(unittest.TestCase):
 
     def test_installed_packages_hashing(self):
-        # We can't really test more than this, testing the exact hash would couple this test to the
-        # projects dependencies so for now this just tests if the function can be called without an error and can return
-        # a str.
+        # Testing the exact hash would couple this test to the projects dependencies.
         package_hash = installed_packages_hash()
         self.assertIsInstance(package_hash, str)
 
