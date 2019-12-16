127db046c5d0d731ec560b2fcef0d781af7971cb
Thales Ribeiro <thalessr@gmail.com>
2019-12-16 15:53:44 -0300

    Renamed class from InitExperiment to VersionTheorem

--- aurum/commands.py
+++ aurum/commands.py
@@ -76,7 +76,7 @@ parser = Parser()
 
 
 @SingletonDecorator
-class InitExperiment:
+class VersionTheorem:
     """
     Singleton class responsible act as a single source of truth regarding the experiment change's state
     Eg. inform that the source-code has changed
@@ -87,29 +87,25 @@ class InitExperiment:
         self.code_changed = False
         self.dataset_changed = False
         self.parameters_changed = False
-        self.metrics_changed = False
 
     def has_any_change(self):
         return self.requirements_changed or self.code_changed or self.dataset_changed or self.parameters_changed \
                or self.metrics_changed
 
-    def requirements_did_change(self):
-        self.requirements_changed = True
+    def requirements_did_change(self, requirements_metadata_location_hash: str) -> None:
+        self.requirements_changed = requirements_metadata_location_hash
 
-    def code_did_change(self):
-        self.code_did_change = True
+    def code_did_change(self, code_metadata_location_hash: str) -> None:
+        self.code_changed = code_metadata_location_hash
 
-    def dataset_did_change(self):
-        self.dataset_changed = True
+    def dataset_did_change(self, dataset_metadata_location_hash: str) -> None:
+        self.dataset_changed = dataset_metadata_location_hash
 
-    def parameters_did_change(self):
-        self.parameters_changed = True
-
-    def metrics_did_change(self):
-        self.metrics_changed = True
+    def parameters_did_change(self, parameters_metadata_location_hash: str) -> None:
+        self.parameters_changed = parameters_metadata_location_hash
 
 
-init = InitExperiment()
+init = VersionTheorem()
 
 
 def run_init(parser: argparse.Namespace) -> None:
