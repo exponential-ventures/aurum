a947f8a458d946a4302a8fb7c6c6f5463fc21257
Nathan Martins <nathan.eua@gmail.com>
2019-12-17 17:52:19 -0300

    Adding parent behaviour.

--- aurum/metadata/experiment.py
+++ aurum/metadata/experiment.py
@@ -1,5 +1,6 @@
 import os
 
+from aurum import constants as cons
 from aurum import git
 from aurum.metadata import MetaData
 from aurum.utils import gen_file_hash
@@ -16,7 +17,28 @@ class ExperimentMetaData(MetaData):
         self.logging_hash = None
 
     def save(self, destination: str) -> str:
+
         self.file_hash = gen_file_hash(
             os.path.join(git.get_git_repo_root(), self.file_name)
         )
+
+        parent = self.get_parent()
+
+        if parent:
+            self.parent_hash = parent.file_hash
+
         return super().save(destination)
+
+    def get_parent(self):
+        newest = None
+        meta_data_dir = os.path.join(git.get_git_repo_root(), cons.REPOSITORY_DIR, cons.EXPERIMENTS_METADATA_DIR)
+
+        for file in os.listdir(meta_data_dir):
+
+            full_path = os.path.join(meta_data_dir, file)
+
+            emd = ExperimentMetaData(full_path)
+            if emd.name == self.name and emd.timestamp > self.timestamp:
+                newest = emd
+
+        return newest
