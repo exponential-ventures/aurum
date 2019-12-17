e6f1091eb8afe1aafb47e884acb44974d3e48647
Nathan Martins <nathan.eua@gmail.com>
2019-12-17 17:10:46 -0300

    renaming...

--- aurum/metadata/__init__.py
+++ aurum/metadata/__init__.py
@@ -1,5 +1,5 @@
 from .metadata import MetaData, gen_meta_hash, gen_meta_file_name_from_hash
-from .dataset_meta_data import DatasetMetaData, get_dataset_metadata
-from .metrics_metadata import MetricsMetaData
-from .parameters_metadata import ParameterMetaData
-from .requirements_meta_data import RequirementsMetaData, get_latest_rmd
+from .dataset import DatasetMetaData, get_dataset_metadata
+from .metrics import MetricsMetaData
+from .parameters import ParameterMetaData
+from .requirements import RequirementsMetaData, get_latest_rmd
