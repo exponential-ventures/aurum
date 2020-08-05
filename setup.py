cfe2d5b47bf0f434663f1e1265ff2edbda008cea
Thales Ribeiro <thalessr@gmail.com>
2020-08-05 14:44:41 +0300

    Add experiment run time in seconds

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='aurum',
-    version='0.1.4',
+    version='0.1.15',
     description='Data and Code Versioning for Data Scientists',
     author='Adriano Marques, Nathan Martins, Thales Ribeiro',
     author_email='adriano@xnv.io, nathan@xnv.io, thales@xnv.io',
