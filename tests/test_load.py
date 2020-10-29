117aa5338e1d0ae38df0a480b013f329a71712bd
Nathan M <nathan.eua@gmail.com>
2020-10-29 16:39:40 -0300

    Formatting.

--- tests/test_load.py
+++ tests/test_load.py
@@ -1,25 +1,25 @@
 #!/usr/bin/env python3
-##
-## Authors: Adriano Marques
-##          Nathan Martins
-##          Thales Ribeiro
-##
-## Copyright (C) 2019 Exponential Ventures LLC
-##
-##    This library is free software; you can redistribute it and/or
-##    modify it under the terms of the GNU Library General Public
-##    License as published by the Free Software Foundation; either
-##    version 2 of the License, or (at your option) any later version.
-##
-##    This library is distributed in the hope that it will be useful,
-##    but WITHOUT ANY WARRANTY; without even the implied warranty of
-##    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-##    Library General Public License for more details.
-##
-##    You should have received a copy of the GNU Library General Public
-##    License along with this library; if not, write to the Free Software
-##    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
-##
+#
+# Authors: Adriano Marques
+#          Nathan Martins
+#          Thales Ribeiro
+#
+# Copyright (C) 2019 Exponential Ventures LLC
+#
+#    This library is free software; you can redistribute it and/or
+#    modify it under the terms of the GNU Library General Public
+#    License as published by the Free Software Foundation; either
+#    version 2 of the License, or (at your option) any later version.
+#
+#    This library is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    Library General Public License for more details.
+#
+#    You should have received a copy of the GNU Library General Public
+#    License along with this library; if not, write to the Free Software
+#    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+#
 
 
 import argparse
@@ -31,7 +31,7 @@ import uuid
 
 from aurum import Theorem, is_new_requirements, end_experiment, commands
 from aurum.code_tracker import is_new_code
-from tests.utils import set_git_for_test
+from tests import set_git_for_test
 
 
 class LoadTestCase(unittest.TestCase):
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,3 +1,26 @@
+#!/usr/bin/env python3
+#
+# Authors: Adriano Marques
+#          Nathan Martins
+#          Thales Ribeiro
+#
+# Copyright (C) 2019 Exponential Ventures LLC
+#
+#    This library is free software; you can redistribute it and/or
+#    modify it under the terms of the GNU Library General Public
+#    License as published by the Free Software Foundation; either
+#    version 2 of the License, or (at your option) any later version.
+#
+#    This library is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    Library General Public License for more details.
+#
+#    You should have received a copy of the GNU Library General Public
+#    License along with this library; if not, write to the Free Software
+#    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+#
+
 import json
 import os
 import shutil
@@ -25,7 +48,6 @@ class TestModel(unittest.TestCase):
         shutil.rmtree(self.repository_path, ignore_errors=True)
 
     def test_save_binary(self):
-
         magic_d = {'sample_field_a': 'sample value a', 'sample_field_b': 'sample value b'}
 
         wmd = WeightsMetaData()
