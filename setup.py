57c1def17923d73cfa4aa8d9952bdad5db18560c
Nathan M <nathan.eua@gmail.com>
2020-11-02 17:44:01 -0300

    Bumping version.

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='aurum',
-    version='0.2.1',
+    version='0.3.0',
     description='Data and Code Versioning for Data Scientists',
     author='Adriano Marques, Nathan Martins, Thales Ribeiro',
     author_email='adriano@xnv.io, nathan@xnv.io, thales@xnv.io',
